830b71547ac183a2d2865d1805adb87d4cde8bd4
Luciano Ferreira Campos <luciano.fecampos@gmail.com>
2018-07-01 17:19:13 -0300

    Implementações

---
+++ src/main/java/br/com/htecweb/workshopmongo/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package br.com.htecweb.workshopmongo.repository;
+
+import br.com.htecweb.workshopmongo.domain.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+}
---
+++ src/main/java/br/com/htecweb/workshopmongo/services/UserService.java
@@ -0,0 +1,52 @@
+package br.com.htecweb.workshopmongo.services;
+
+import br.com.htecweb.workshopmongo.domain.User;
+import br.com.htecweb.workshopmongo.dto.UserDTO;
+import br.com.htecweb.workshopmongo.repository.UserRepository;
+import br.com.htecweb.workshopmongo.services.exception.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.validation.constraints.NotNull;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repo;
+
+    public List<User> findAll() {
+        return repo.findAll();
+    }
+
+    public User findById(String id) {
+        Optional<User> obj = repo.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
+    public User insert(User obj) {
+        return repo.insert(obj);
+    }
+
+    public void delete(String id) {
+        findById(id);
+        repo.deleteById(id);
+    }
+
+    public User update(User obj) {
+        User newObj = findById(obj.getId());
+        updateData(newObj, obj);
+        return repo.save(newObj);
+    }
+
+    private void updateData(@NotNull User newObj, @NotNull User obj) {
+        newObj.setName(obj.getName());
+        newObj.setEmail(obj.getEmail());
+    }
+
+    public User fromDTO(UserDTO objDto) {
+        return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+    }
+}
